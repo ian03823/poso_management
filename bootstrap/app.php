2125807c46123aaced96294c825a288fe0b7ad05
CrisIan <iancarino383@gmail.com>
2025-04-13 22:28:58 +0800

    Added middleware of three entity

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +12,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // 
+        $middleware->group('admin', [
+            \App\Http\Middleware\AdminMiddleware::class,
+        ]);
+        
+        $middleware->group('enforcer', [
+            \App\Http\Middleware\EnforcerMiddleware::class,
+        ]);
+        $middleware->group('violator', [
+            \App\Http\Middleware\ViolatorMiddleware::class,
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
