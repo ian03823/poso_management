b77963a7586f2e120121e771508440f697dd0585
CrisIan <iancarino383@gmail.com>
2025-04-13 22:28:23 +0800

    Added logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,7 @@ class AuthController extends Controller
 
         return view("auth.login");
     }
+
     public function login(Request $request){
 
         $validated = $request->validate([
@@ -22,15 +23,21 @@ class AuthController extends Controller
             "password"=> "required|string"
         ]);
         
-        if(Auth::attempt($validated)){
-
-            $request->session()->regenerate();  
-            return redirect()->route('admin.index');
+          if (Auth::guard('admin')->attempt($validated)) {
+            $request->session()->regenerate();  // Regenerate session
+            return redirect()->route('admin.dashboard'); // Redirect to admin dashboard
         }
 
         throw ValidationException::withMessages([
             'credentials'=> 'Incorrect password or username.'
         ]);
+    }
+    
+    public function logout(Request $request){
+        Auth::guard('admin')->logout();
+        $request->session()->regenerate();
+        $request->session()->regenerateToken();
+        return redirect()->route('admin.showLogin');
     }
 }
 
