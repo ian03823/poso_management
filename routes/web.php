273546e573a84289bc59aea19e9f91fe6b2bbe56
CrisIan <iancarino383@gmail.com>
2025-04-13 22:31:42 +0800

    Added middleware routes

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PosoManagementController;
+use App\Http\Controllers\AdminManagementController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AddEnforcer;
+use App\Http\Controllers\EnforcerAcc;
+use App\Http\Controllers\ViolationController;
+use App\Http\Controllers\EnforcerAuthController;
+use App\Http\Controllers\EnforcerManagementController;
+use App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('pmc', PosoManagementController::class);    
+Route::get('/alogin', [AuthController::class, 'showLogin'])->name('admin.showLogin');
+Route::post('/alogin', [AuthController::class, 'login'])->name('admin.login');
+Route::post('/alogout', [AuthController::class, 'logout'])->name('admin.logout');
+
+// Enforcer login routes
+Route::get('/plogin', [EnforcerAuthController::class, 'showLogin'])->name('enforcer.showLogin');
+Route::post('/plogin', [EnforcerAuthController::class,'login'])->name('enforcer.login');
+Route::post('/plogout', [EnforcerAuthController::class,'logout'])->name('enforcer.logout');
+
+// Enforcer protected routes
+Route::middleware('enforcer')->group(function () {
+    Route::get('/pdash', [EnforcerManagementController::class,'enforcerDash'])->name('enforcer.dashboard');
+    Route::resource('enforcerCreate', EnforcerAcc::class);
+    Route::resource('enforcerTicket', TicketController::class);
+    Route::resource('enf', EnforcerManagementController::class);
+});
+
+// Admin protected routes
+Route::middleware('admin')->group(function () {
+    Route::get('/admin', [AdminManagementController::class,'adminDash'])->name('admin.dashboard');
+    Route::resource('violation', ViolationController::class); 
+    Route::resource('enforcer', AddEnforcer::class);
+});
 
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-Route::post('/login', [AuthController::class,'login'])->name('login');
-
-
-route::get('/admin', [PosoManagementController::class,'index'])->name('admin.index');
-route::get('/admin/create', [PosoManagementController::class,'create'])->name('admin.create');
-route::get('/admin/show', [PosoManagementController::class,'show'])->name('admin.show');
-
-// Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
